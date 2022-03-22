09d7d31de73fb359969aeb583dc46cf7d7dc94d7
Noel <noel.huberto@gmail.com>
2022-03-22 10:52:13 +0100

    Backend module added

---
+++ SportsNoteAPI/src/main/java/fr/noelh/sportsnoteapi/SportsNoteApiApplication.java
@@ -0,0 +1,13 @@
+package fr.noelh.sportsnoteapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsNoteApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportsNoteApiApplication.class, args);
+    }
+
+}
---
+++ SportsNoteAPI/src/test/java/fr/noelh/sportsnoteapi/SportsNoteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.noelh.sportsnoteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsNoteApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
