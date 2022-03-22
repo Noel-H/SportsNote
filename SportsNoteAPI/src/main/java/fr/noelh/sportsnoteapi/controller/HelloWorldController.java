2fa8b61058224cfb8e093a88fa1fdced671c4b4c
Noel <noel.huberto@gmail.com>
2022-03-22 23:56:00 +0100

    Cors configured

--- SportsNoteAPI/src/main/java/fr/noelh/sportsnoteapi/controller/HelloWorldController.java
+++ SportsNoteAPI/src/main/java/fr/noelh/sportsnoteapi/controller/HelloWorldController.java
@@ -3,14 +3,12 @@ package fr.noelh.sportsnoteapi.controller;
 import fr.noelh.sportsnoteapi.dto.HelloWorldDTO;
 import fr.noelh.sportsnoteapi.service.HelloWorldService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
-@CrossOrigin(origins = "http://localhost:4200")
 public class HelloWorldController {
 
     @Autowired
