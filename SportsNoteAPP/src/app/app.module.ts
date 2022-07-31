9fbb012f94c00214fc4e863e168ff0dce09075ad
Noel <noel.huberto@gmail.com>
2022-07-31 17:42:39 +0200

    WIP : WeightRecordList view - NavBar updated

--- SportsNoteAPP/src/app/app.module.ts
+++ SportsNoteAPP/src/app/app.module.ts
@@ -7,6 +7,12 @@ import { HelloWorldComponent } from './hello-world/hello-world.component';
 import { WeightComponent } from './weight/weight.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { AverageWeightComponent } from './average-weight/average-weight.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
+
 
 @NgModule({
   declarations: [
@@ -19,6 +25,10 @@ import { AverageWeightComponent } from './average-weight/average-weight.componen
   imports: [
     BrowserModule,
     HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
