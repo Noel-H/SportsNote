7e9bf071469ccbcfebf0147a3aa04c2013c344c0
Noel-H <noel.huberto@gmail.com>
2022-10-01 15:53:58 +0200

    WIP : dialog event action. service refactored

--- SportsNoteAPP/src/app/service/weight-record-list.service.ts
+++ SportsNoteAPP/src/app/service/weight-record-list.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
-import {catchError, Observable, of, tap} from "rxjs";
+import {Observable} from "rxjs";
 import {WeightRecordDTO} from "../interface/weight-record-d-t-o";
 
 @Injectable({
@@ -11,53 +11,26 @@ export class WeightRecordListService {
   constructor(private http: HttpClient) { }
 
   getWeightRecordList(): Observable<WeightRecordDTO[]>{
-    return this.http.get<WeightRecordDTO[]>('http://localhost:8080/weight_record/list').pipe(
-      tap((weightRecordList) => console.table(weightRecordList)),
-      catchError((error) => {
-        console.log(error);
-        return of([])
-      })
-    );
+    return this.http.get<WeightRecordDTO[]>('http://localhost:8080/weight_record/list');
   }
 
   addWeightRecord(weightRecordDTO : WeightRecordDTO): Observable<WeightRecordDTO>{
     return this.http.post<WeightRecordDTO>('http://localhost:8080/weight_record', weightRecordDTO);
   }
 
-  updateWeightRecord(weightRecordDTO : WeightRecordDTO): Observable<WeightRecordDTO|undefined>{
-    const httpOptions = {
-      headers : new HttpHeaders({'Content-Type' : 'application/json'})
-    };
-    return this.http.put<WeightRecordDTO>('http://localhost:8080/weight_record', weightRecordDTO, httpOptions).pipe(
-      tap((response) => console.table(response)),
-      catchError((error) => {
-        console.log(error);
-        return of(undefined)
-      })
-    );
+  updateWeightRecord(weightRecordDTO : WeightRecordDTO): Observable<WeightRecordDTO>{
+    return this.http.put<WeightRecordDTO>('http://localhost:8080/weight_record', weightRecordDTO);
   }
 
-  deleteWeightRecord(weightRecordDTO : WeightRecordDTO): Observable<WeightRecordDTO|undefined>{
+  deleteWeightRecord(weightRecordDTO : WeightRecordDTO): Observable<WeightRecordDTO>{
     const httpOptions = {
       headers : new HttpHeaders({'Content-Type' : 'application/json'}),
       body : weightRecordDTO
     };
-    return this.http.delete<WeightRecordDTO>('http://localhost:8080/weight_record', httpOptions).pipe(
-      tap((response) => console.table(response)),
-      catchError((error) => {
-        console.log(error);
-        return of(undefined)
-      })
-    );
+    return this.http.delete<WeightRecordDTO>('http://localhost:8080/weight_record', httpOptions);
   }
 
   getAverageWeightRecordList(): Observable<WeightRecordDTO[]>{
-    return this.http.get<WeightRecordDTO[]>('http://localhost:8080/average_weight_record/list').pipe(
-      tap((averageWeightRecordList) => console.table(averageWeightRecordList)),
-      catchError((error) => {
-        console.log(error);
-        return of([])
-      })
-    );
+    return this.http.get<WeightRecordDTO[]>('http://localhost:8080/average_weight_record/list');
   }
 }
